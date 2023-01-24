650a834cd842fc8a41dc790bf0da865126dff96c
Francesco Vasco <francesco.vasco@lightstreamer.com>
2023-01-24 14:45:03 +0100

    Allowlist support

--- sort.sh
+++ sort.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for f in ???; do cp $f /tmp/forbidden-words.tmp; LANG=C sort -u /tmp/forbidden-words.tmp > $f; done
+for f in ??? allowlist/???; do cp $f /tmp/forbidden-words.tmp; LANG=C sort -u /tmp/forbidden-words.tmp > $f; done
